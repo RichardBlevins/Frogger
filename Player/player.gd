07d12c77f2a01f28be90aa7e9d30b43e6541ec7b
Noah <107080597+RichardBlevins@users.noreply.github.com>
2026-01-09 15:22:31 -0800

    Implemented start animation
    frog moves from the side to the middle of the screen before the game starts

--- Player/player.gd
+++ Player/player.gd
@@ -1,55 +1,64 @@
 extends CharacterBody2D
 
-enum state { DEAD }
+enum STATE {PLAY, ANIMATION}
 
 const GRID = 32
 var cooldown = false
+@export var lives = 3
+
 @onready var raycast = $RayCast
 @onready var frogPivot = $Pivot
 
 @onready var frogidle = $Pivot/FrogwizootteIdle
 @onready var frogjumping = $Pivot/Frogwizootte1
 
-@export var State = state
+func _ready() -> void:
+	for intro in 7:
+		await get_tree().create_timer(0.4).timeout
+		position.x += GRID
+		print(position)
+		hop_animation()
+	process_mode = Node.PROCESS_MODE_INHERIT
+		
 
 func _physics_process(_delta: float) -> void:
-	if state.DEAD:
-		process_mode = Node.PROCESS_MODE_DISABLED
-
-	var direction = Vector2(
-		Input.get_axis("ui_left", "ui_right"),
-		Input.get_axis("ui_up", "ui_down")
-	)
-	
-	var illdirectionlist = [Vector2.ZERO, Vector2.ONE, -Vector2.ONE, Vector2(1, -1), Vector2(-1, 1)]
-	
-	# Only move if a key was just pressed
-	if direction not in illdirectionlist and cooldown == false and (
-		Input.is_action_just_pressed("ui_right") or
-		Input.is_action_just_pressed("ui_left") or
-		Input.is_action_just_pressed("ui_up") or
-		Input.is_action_just_pressed("ui_down")
-	):
+		var direction = Vector2(
+			Input.get_axis("ui_left", "ui_right"),
+			Input.get_axis("ui_up", "ui_down")
+		)
 		
-		cooldown = true
-		raycast.target_position = direction * GRID #RAYCAST ROTATION
-		frogPivot.look_at(raycast.target_position + position)#FROG ROTATION
-		position += direction * GRID #MOVMENT
-		frogidle.visible = false
-		frogjumping.visible = true
-		await get_tree().create_timer(0.1).timeout
-		frogidle.visible = true
-		frogjumping.visible = false
-
+		var illdirectionlist = [Vector2.ZERO, Vector2.ONE, -Vector2.ONE, Vector2(1, -1), Vector2(-1, 1)]
 		
-		cooldown = false
-	
-	move_and_slide()
-
-
-
+		# Only move if a key was just pressed
+		if direction not in illdirectionlist and cooldown == false and (
+			Input.is_action_just_pressed("ui_right") or
+			Input.is_action_just_pressed("ui_left") or
+			Input.is_action_just_pressed("ui_up") or
+			Input.is_action_just_pressed("ui_down")
+		):
+			
+			cooldown = true
+			raycast.target_position = direction * GRID                #RAYCAST ROTATION
+			frogPivot.look_at(raycast.target_position + position)     #FROG ROTATION
+			position += direction * GRID                            #MOVMENT
+			hop_animation()
+			await get_tree().create_timer(0.1).timeout  
+			cooldown = false
+		
+		move_and_slide()
 
 
 func _screen_exited() -> void:
-	
-	Manager.Death()
+	if lives <= 0:
+		Manager.Death() #this is basically final death game over
+	else:
+		print(lives)
+		lives -= 1
+		#this die and den respawn
+
+func hop_animation():
+	frogidle.visible = false 
+	frogjumping.visible = true
+	await get_tree().create_timer(0.1).timeout
+	frogidle.visible = true
+	frogjumping.visible = false
